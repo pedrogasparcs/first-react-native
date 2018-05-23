9db96aac65cde0220a4b482db2cc3bd5dc4a0fd6
Formador <form@s4macform.local>
2018-05-23 20:01:20 +0100

    Intro & Environment Setup

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+
+const Agradecimento = ({pessoa}) => {
+  return <View>
+    <Text>Olá! {pessoa}</Text>
+  </View>
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.textStyle}>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu. asdfasdf</Text>
+          <Image source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'}} 
+                style={{width: 100, height: 100}}/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+          <Agradecimento pessoa="Jorge"/>
+          <Agradecimento pessoa="Tiago"/>
+          <Agradecimento pessoa="Mariana"/>
+          <Agradecimento pessoa="Xavier"/>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 
 const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 16
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
